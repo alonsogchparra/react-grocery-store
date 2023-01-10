ec439ad5428baa580bb76671556aefd1f1c404c8
Alonso Parra <alonsogparra@gmail.com>
2023-01-10 15:40:28 -0400

    index updated with animate.css, sass and redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import 'animate.css';
+import './styles/styles.scss';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
