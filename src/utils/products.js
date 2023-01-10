93879b29c7cc9170510d3d77428ede1867f31eed
Alonso Parra <alonsogparra@gmail.com>
2023-01-10 14:44:21 -0400

    array object simulate the api get info of products

---
+++ src/utils/products.js
@@ -0,0 +1,63 @@
+export const content = [
+  {
+    name: 'Leche',
+    price: 75000,
+    amount: 2,
+    id: 1,
+  },
+  {
+    name: 'Cereal',
+    price: 85300,
+    amount: 3,
+    id: 2,
+  },
+  {
+    name: 'Huevos',
+    price: 75000,
+    amount: 12,
+    id: 3,
+  },
+  {
+    name: 'Carne',
+    price: 85300,
+    amount: 21,
+
+    id: 4,
+  },
+  {
+    name: 'Queso',
+    price: 75000,
+    amount: 8,
+    id: 5,
+  },
+  {
+    name: 'Arroz',
+    price: 85300,
+    amount: 9,
+    id: 6,
+  },
+  {
+    name: 'Frijoles',
+    price: 85300,
+    amount: 32,
+    id: 7,
+  },
+  {
+    name: 'Lentejas',
+    price: 175000,
+    amount: 42,
+    id: 8,
+  },
+  {
+    name: 'Avena',
+    price: 85300,
+    amount: 56,
+    id: 9,
+  },
+  {
+    name: 'Pollo',
+    price: 375000,
+    amount: 23,
+    id: 10,
+  },
+];
