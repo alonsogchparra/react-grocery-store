823b585d58f3da954814c7861eb85ef3fee13831
Alonso Parra <alonsogparra@gmail.com>
2023-01-10 15:39:17 -0400

    Router added in App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Cart } from './components/Cart';
+import { Navbar } from './components/Navbar';
+import { Products } from './components/Products';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router basename='/'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Products />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
